8e17c9339a995636ccfc870166c315c93b0da5dc
Nick Reed <nicholas.a.reed@gmail.com>
2016-04-26 11:09:39 -0700

    using SDK

--- embed_the_console/embed_the_console.js
+++ embed_the_console/embed_the_console.js
@@ -1,96 +1,95 @@
-//
-// to run this sample
-//  1. copy the file in your own directory - say, example.js
-//  2. change "***" to appropriate values
-//  3. install async and request packages
-//     npm install async
-//     npm install request
-//  4. execute
-//     node example.js 
+// Request Console View (Node.js)
 // 
+// To run this sample
+//  1. Copy the file to your local machine and give .js extension (i.e. example.js)
+//  2. Change "***" to appropriate values
+//  3. Install docusign-esign and async packages
+//     npm install docusign-esign
+//     npm install async
+//  4. Execute
+//     node example.js 
 
-var   	async = require("async"),		// async module
-	request = require("request"),		// request module
-	email = "***",				// your account email
-	password = "***",			// your account password
-	integratorKey = "***",			// your account Integrator Key (found on Preferences -> API page)
-	baseUrl = "";				// we will retrieve this
+
+var docusign = require('docusign-esign'),
+	async = require('async');
+
+var integratorKey = process.env.DOCUSIGN_INTEGRATOR_KEY || '***',	// Integrator Key associated with your DocuSign Integration
+	email = process.env.DOCUSIGN_LOGIN_EMAIL || '***',				// Email for your DocuSign Account
+	password = process.env.DOCUSIGN_LOGIN_PASSWORD || '***',		// Password for your DocuSign Account
+	docusignEnv = 'demo',	// DocuSign Environment generally demo for testing purposes ('www' == production)
+	baseUrl = 'https://' + docusignEnv + '.docusign.net/restapi';
 
 async.waterfall(
-[
-	//////////////////////////////////////////////////////////////////////
-	// Step 1 - Login (used to retrieve accountId and baseUrl)
-	//////////////////////////////////////////////////////////////////////
-	function(next) {
-		var url = "https://demo.docusign.net/restapi/v2/login_information";
-		var body = "";	// no request body for login api call
-		
-		// set request url, method, body, and headers
-		var options = initializeRequest(url, "GET", body, email, password);
-		
-		// send the request...
-		request(options, function(err, res, body) {
-			if(!parseResponseBody(err, res, body)) {
+  [
+	/////////////////////////////////////////////////////////////////////////////////////
+	// Step 1: Login (used to retrieve your accountId and account baseUrl)
+	/////////////////////////////////////////////////////////////////////////////////////
+
+	function login(next) {
+
+		// initialize the api client
+		var apiClient = new docusign.ApiClient();
+		apiClient.setBasePath(baseUrl);
+
+		// create JSON formatted auth header
+		var creds = JSON.stringify({
+		  Username: email,
+		  Password: password,
+		  IntegratorKey: integratorKey
+		});
+		apiClient.addDefaultHeader('X-DocuSign-Authentication', creds);
+
+		// assign api client to the Configuration object
+		docusign.Configuration.default.setDefaultApiClient(apiClient);
+
+		// login call available off the AuthenticationApi
+		var authApi = new docusign.AuthenticationApi();
+
+		// login has some optional parameters we can set
+		var loginOps = new authApi.LoginOptions();
+		loginOps.setApiPassword('true');
+		loginOps.setIncludeAccountIdGuid('true');
+		authApi.login(loginOps, function (err, loginInfo, response) {
+			if (err) {
+				console.error(err.response ? err.response.error : err);
 				return;
 			}
-			baseUrl = JSON.parse(body).loginAccounts[0].baseUrl;
-			next(null); // call next function
+			if (loginInfo) {
+				// list of user account(s)
+				// note that a given user may be a member of multiple accounts
+				var loginAccounts = loginInfo.getLoginAccounts();
+				console.log('LoginInformation: ' + JSON.stringify(loginAccounts));
+				next(null, loginAccounts);
+			}
 		});
 	},
-	
-	//////////////////////////////////////////////////////////////////////
-	// Step 2 - Get the Authenticated DocuSign Console view
-	//////////////////////////////////////////////////////////////////////
-	function(next) {
-		var url = baseUrl + "/views/console";
-		var body = "";
-		
-		// set request url, method, and body
-		var options = initializeRequest(url, "POST", body, email, password);
-		
-		// send the request...
-		request(options, function(err, res, body) {
-			if(!parseResponseBody(err, res, body))
+
+	// ===============================================================================
+	// Step 2:  Create ConsoleView 
+	// ===============================================================================
+    function createConsoleView(loginAccounts, next){
+		var loginAccount = new docusign.LoginAccount();
+		loginAccount = loginAccounts[0];
+		var accountId = loginAccount.accountId;
+
+		// instantiate a new envelopesApi object
+		var envelopesApi = new docusign.EnvelopesApi();
+
+		// set the url where you want the user to go once they logout of the Console
+		var returnUrl = new docusign.ConsoleViewRequest();
+		returnUrl.setReturnUrl('https://www.docusign.com/devcenter');
+
+		// call the createConsoleView() API
+		envelopesApi.createConsoleView(accountId, returnUrl, function (error, consoleView, response) {
+			if (error) {
+				console.log('Error: ' + error);
 				return;
-			else
-				console.log("\nNavigate to the above URL to open the DocuSign Console...");
+			}
+
+			if (consoleView) {
+				console.log('ConsoleView: ' + JSON.stringify(consoleView));
+			}
 		});
 	}
+
 ]);
-
-//***********************************************************************************************
-// --- HELPER FUNCTIONS ---
-//***********************************************************************************************
-function initializeRequest(url, method, body, email, password) {	
-	var options = {
-		"method": method,
-		"uri": url,
-		"body": body,
-		"headers": {}
-	};
-	addRequestHeaders(options, email, password);
-	return options;
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////
-function addRequestHeaders(options, email, password) {	
-	// JSON formatted authentication header (XML format allowed as well)
-	dsAuthHeader = JSON.stringify({
-		"Username": email,
-		"Password": password, 
-		"IntegratorKey": integratorKey	// global
-	});
-	// DocuSign authorization header
-	options.headers["X-DocuSign-Authentication"] = dsAuthHeader;
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////
-function parseResponseBody(err, res, body) {
-	console.log("\r\nAPI Call Result: \r\n", JSON.parse(body));
-	if( res.statusCode != 200 && res.statusCode != 201)	{ // success statuses
-		console.log("Error calling webservice, status is: ", res.statusCode);
-		console.log("\r\n", err);
-		return false;
-	}
-	return true;
-}
